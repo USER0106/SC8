7a6d16811299aaab57236394f7c0c29fa36028fc
Amirr <107915452+USER0106@users.noreply.github.com>
2022-08-15 14:29:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6281511480762'],
-  ['6281511480762', 'YahyaGanzz', true]
+  ['6282154156241'],
+  ['6282154156241', '𝘼𝙈𝙄𝙍𝙍', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -41,8 +41,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'AzzBot-Md'
-  var sticker_author = 'YahyaGanzz'
+  var sticker_name = '𝙍𝙐𝙆𝘼 - 𝘽𝙊𝙏'
+  var sticker_author = '𝘼𝙈𝙄𝙍𝙍'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -59,7 +59,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = 'AzzBot-Yahya'
+global.wm = '𝙍𝙐𝙆𝘼 - 𝘽𝙊𝙏 𝙓 𝘼𝙈𝙄𝙍𝙍'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
